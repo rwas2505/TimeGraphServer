087c22b64bba27242d2a7d45eb16917e624adba1
Ryan <ryanwaszak@gmail.com>
2021-03-28 12:45:40 -0500

    configure in-memory db and populate with seed data

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +18,10 @@ namespace TimeGraphServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TimeGraphContext>(context =>
+            {
+            context.UseInMemoryDatabase("TimeGraphServer");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
