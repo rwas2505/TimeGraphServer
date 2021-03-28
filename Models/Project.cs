30760e156723f980afd9bf80f852e5afb1c5a24c
Ryan <ryanwaszak@gmail.com>
2021-03-28 12:33:48 -0500

    initial commit with models created

---
+++ Models/Project.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
